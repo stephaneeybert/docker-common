d9d7379238be2e6c1bf158c6f0f95ca0ee385248
Stephane Eybert <mittiprovence@yahoo.se>
2019-02-05 16:50:34 +0200

    Added a version number to the image.

--- redis/build.sh
+++ redis/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 
-docker build -t stephaneeybert/redis --rm=true .
+docker build -t stephaneeybert/redis:3.0.7 --rm=true .
 if [ $? -eq 0 ]; then
-docker tag stephaneeybert/redis localhost:5000/redis
-docker push localhost:5000/redis
-docker tag stephaneeybert/redis thalasoft.com:5000/redis
-docker push thalasoft.com:5000/redis
+docker tag stephaneeybert/redis:3.0.7 localhost:5000/redis:3.0.7
+docker push localhost:5000/redis:3.0.7
+docker tag stephaneeybert/redis:3.0.7 thalasoft.com:5000/redis:3.0.7
+docker push thalasoft.com:5000/redis:3.0.7
 fi
