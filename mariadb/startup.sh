0946dfbd44ec3a3607a8fbc0a1fe4465bc533935
Stephane Eybert <mittiprovence@yahoo.se>
2019-12-07 11:14:44 +0200

    Added some comments.

--- mariadb/startup.sh
+++ mariadb/startup.sh
@@ -7,7 +7,9 @@ chgrp -R $HOST_GROUP_ID /usr/local/mariadb/install/data
 export DB_ROOT_PASSWORD={{DOCKER-SECRET:DB_ROOT_PASSWORD}}
 source /usr/local/mariadb/expand-secrets.sh
 
+# Check if the database server has not yet been fully installed
 if [ ! -f /usr/local/mariadb/install/data/ibdata1 ]; then
+  # Do the post installation
   chmod 755 /usr/local/mariadb/install/scripts/mysql_install_db
   sleep 1 # Needed to avoid running the script when this mysql_install_db file has not yet been released by the permissions change
   /usr/local/mariadb/install/scripts/mysql_install_db \
@@ -18,8 +20,11 @@ if [ ! -f /usr/local/mariadb/install/data/ibdata1 ]; then
     --tmpdir=/usr/local/mariadb/install/tmp \
     --lc-messages-dir=/usr/local/mariadb/install/share
 
+  # Start the database server
   /usr/local/mariadb/install/bin/mysqld_safe --defaults-file=/usr/local/mariadb/install/my.cnf &
   sleep 10s
+
+  # Configure the root user
   /usr/local/mariadb/install/bin/mysql -u root -v -e "grant all on *.* to root@'%' identified by '$DB_ROOT_PASSWORD' with grant option; flush privileges;"
   # The % does not include localhost so the same command has to be executed for specific hosts 
   /usr/local/mariadb/install/bin/mysql -u root -v -e "grant all on *.* to root@'mysql' identified by '$DB_ROOT_PASSWORD' with grant option; flush privileges;"
@@ -29,6 +34,7 @@ if [ ! -f /usr/local/mariadb/install/data/ibdata1 ]; then
   # But only after having set the root password
   export MYSQL_PWD=$DB_ROOT_PASSWORD
 
+  # Remove any existing default users
   /usr/local/mariadb/install/bin/mysql -u root -v -e "use mysql; delete from user where User = ''; flush privileges;"
 
   # tail -f /etc/hosts
@@ -36,5 +42,6 @@ else
   # Avoid having to provide the user password on the command line
   export MYSQL_PWD=$DB_ROOT_PASSWORD
 
+  # Start the database server
   /usr/local/mariadb/install/bin/mysqld_safe --defaults-file=/usr/local/mariadb/install/my.cnf
 fi
