4abaab9331a95b7f0673f09e4bae02a9d9af13f6
Stephane Eybert <mittiprovence@yahoo.se>
2019-12-23 11:58:20 +0200

    Removed some unused images.

--- alpineos-mariadb/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-docker build -t stephaneeybert/alpineos-mariadb:10.3.7 --rm=true .
-if [ $? -eq 0 ]; then
-docker tag stephaneeybert/alpineos-mariadb:10.3.7 localhost:5000/alpineos-mariadb:10.3.7
-docker push localhost:5000/alpineos-mariadb:10.3.7
-docker tag stephaneeybert/alpineos-mariadb:10.3.7 thalasoft.com:5000/alpineos-mariadb:10.3.7
-docker push thalasoft.com:5000/alpineos-mariadb:10.3.7
-fi
