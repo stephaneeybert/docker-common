a8b315fa3eb8cc907798d6aa3aa4e1073f064ef2
Stephane Eybert <mittiprovence@yahoo.se>
2021-05-21 22:44:10 +0200

    Removed the non necessary property that caused a fail on a unknown user.

--- logrotate/build.sh
+++ logrotate/build.sh
@@ -5,5 +5,7 @@ if [ $? -eq 0 ]; then
 docker tag stephaneeybert/logrotate localhost:5000/logrotate
 docker push localhost:5000/logrotate
 docker tag stephaneeybert/logrotate thalasoft.com:5000/logrotate
+docker tag stephaneeybert/logrotate europasprak.com:5000/logrotate
 #docker push thalasoft.com:5000/logrotate
+docker push europasprak.com:5000/logrotate
 fi
