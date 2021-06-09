799cb70b399963d1ac01db1c18dc9f3346ba4806
Stephane Eybert <mittiprovence@yahoo.se>
2021-06-09 09:41:59 +0200

    Corrected the version of the MySQL image

--- ubuntuos-mysql/build.sh
+++ ubuntuos-mysql/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 
-docker build -t stephaneeybert/ubuntuos-mysql:8.0.15 --rm=true .
+docker build -t stephaneeybert/ubuntuos-mysql:8.0.19 --rm=true .
 if [ $? -eq 0 ]; then
-docker tag stephaneeybert/ubuntuos-mysql:8.0.15 localhost:5000/ubuntuos-mysql:8.0.15
-docker push localhost:5000/ubuntuos-mysql:8.0.15
-docker tag stephaneeybert/ubuntuos-mysql:8.0.15 thalasoft.com:5000/ubuntuos-mysql:8.0.15
-#docker push thalasoft.com:5000/ubuntuos-mysql:8.0.15
+docker tag stephaneeybert/ubuntuos-mysql:8.0.19 localhost:5000/ubuntuos-mysql:8.0.19
+docker push localhost:5000/ubuntuos-mysql:8.0.19
+docker tag stephaneeybert/ubuntuos-mysql:8.0.19 thalasoft.com:5000/ubuntuos-mysql:8.0.19
+#docker push thalasoft.com:5000/ubuntuos-mysql:8.0.19
 fi
